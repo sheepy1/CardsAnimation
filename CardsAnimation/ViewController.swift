829bd444da55c72658d5df916b3971a3cc04cec3
sheepy1 <sheepyyoung@foxmail.com>
2015-09-03 16:00:57 +0800

    Initial Commit

---
+++ CardsAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardsAnimation
+//
+//  Created by Sheepy on 15/9/3.
+//  Copyright (c) 2015年 Sheepy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
